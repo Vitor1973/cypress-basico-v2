9f6a8b7801a50bf32477a72913d73cdda999554d
Vitor Hugo 1973 <devechi.vitor@gmail.com>
2022-08-17 00:54:24 -0300

    Atualização 17 Agosto de 2022

--- cypress/integration/CAC -TAT.spec.js
+++ cypress/integration/CAC -TAT.spec.js
@@ -2,9 +2,20 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o titulo da aplicação', function() {
+    beforeEach(function()  {
         cy.visit('./src/index.html')
+    })
 
+    it('verifica o titulo da aplicação', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+
+    it.only('preenche os campos obrigatórios e envia o formulário', function() {
+        cy.get('#firstName').type('Vitor Hugo')
+        cy.get('#lastName').type('Devechi')
+        cy.get('#email').type('vitorhugo@gmail.com')
+        cy.get('#open-text-area').type('Hoje eu inicio um novo ciclo nesta maravilhosa empresa.')
+        cy.get('button[type="submit"]').click()
+
     })
 })
