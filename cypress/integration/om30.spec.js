5e5a6d5a85994e03d0fc143eeb7620235c611374
Vitor1973 <devechi.vitor@gmail.com>
2023-01-04 23:22:44 -0300

    Alteração 04 01 2023

--- cypress/integration/om30.spec.js
+++ cypress/integration/om30.spec.js
@@ -3,9 +3,9 @@ describe('Login Saude Simples', function() {
         
         cy.visit('https://homologacao.saudesimples.net')
     })
-    it('Entrar na pagina om30', function()  {
+    it.only('Entrar na pagina om30', function()  {
         //cy.get('#usuario_login').type('produtos')
-        cy.get('#usuario_login').type('produtos');
+        cy.get('#usuario_login').type('produtos'); // argumentos
         cy.get('#usuario_password').type('produtos10203040@')
 
 
